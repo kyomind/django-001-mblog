3f7dc3eaa23b801ef2610b141fab1187d922f995
kyo <odinxp@gmail.com>
2020-03-04 14:38:38 +0800

    首頁可看到文章

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_list = list()
+    for count, post in enumerate(posts):
+        post_list.append(f'No. {str(count)} {str(post)} <br>')
+    return HttpResponse(post_list)
